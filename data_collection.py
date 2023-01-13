abea4e0cfc1f75cb2e41f9bc964a24ea973bc8fa
VeronicaHurtado <7286232+VeronicaHurtado@users.noreply.github.com>
2023-01-13 18:11:34 +1100

    Organise functions to be used

--- data_collection.py
+++ data_collection.py
@@ -33,5 +33,25 @@ def get_distance(origins, destinations, output_format='json'):
     data = json.loads(response.text)
 
     return data['rows'][0]['elements'][0]['distance']['text']
-    
 
+
+# Get fuel consumption/efficiency in Kilometres per litre
+def get_fuel_consumption(car_make, car_model):
+    # @ToDo: Get actual API for this function
+    car_api_url = f"https://example.com/api/car_efficiency?make={car_make}&model={car_model}"
+    car_response = requests.get(car_api_url)
+    car_data = json.loads(car_response.text)
+    try:
+        kml = car_data["klm"]
+    except KeyError:
+        raise ValueError(f"No data found for {car_make} {car_model}.")
+    return kml
+
+
+def get_car_emissions_rate(car_make, car_mode):
+    # @ToDo: Use car emissions API to get the emissions rate for a car
+    car_emissions_url = "https://example.com/car_emissions_api"
+    car_emissions_response = requests.get(car_emissions_url)
+    car_emissions_data = json.loads(car_emissions_response.text)
+    car_emissions_rate = car_emissions_data["emissions_rate"]
+    return car_emissions_rate
---
+++ functions.py
@@ -0,0 +1,48 @@
+from data_collection import get_distance, get_car_emissions_rate
+
+
+# Calculate the cost of the trip based on distance, fuel_type and cost_per_litre
+# distance: a float indicating the distance in kilometres for the given trip
+# fuel_type: a string indicating the type of fuel used (either "petrol" or "diesel")
+# cost_per_litre: a float indicating the cost of fuel per litre (in dollars)
+def calculate_fuel_cost(distance, fuel_type, cost_per_litre):
+    # @ToDo: Replace with kilometres per litre based on Car Make and Model from get_fuel_consumption
+    # @ToDo: Consider average speed
+    if fuel_type == "petrol":
+        # Assume fuel consumption is 12.5 kilometres per litre
+        kml = 12.5
+    elif fuel_type == "diesel":
+        # Assume fuel consumption is 15.5 kilometres per litre
+        kml = 15.5
+    else:
+        # Raise an error for unsupported fuel types
+        raise ValueError(f"Unsupported fuel type: {fuel_type}")
+
+    # Calculate the number of litres consumed
+    litres_consumed = distance / kml
+    # Calculate the cost of the fuel consumed
+    fuel_cost = litres_consumed * cost_per_litre
+    return fuel_cost
+
+
+# Calculate the fuel cost between two locations based on fuel type
+# start_location: a string indicating the address for the starting point (e.g. "1 Collins Street, Melbourne, VIC")
+# end_location: a string indicating the address for the ending point (e.g., "1 Elizabeth Street, Melbourne, VIC")
+# fuel_type: a string indicating the type of fuel used (either "petrol" or "diesel")
+def calculate_fuel_cost_between_destinations(start_location, end_location, fuel_type):
+    # @ToDo: Get car_make, car_model,
+
+    # @ToDo: 1 - Get fuel cost per litre from the API
+    cost_per_litre = 2.50  # Assume this price for now
+    # 2 - Get distance in Kilometres between given locations
+    distance = get_distance(origins=start_location, destinations=end_location)
+    # 3 - Calculate total fuel cost for this distance
+    cost = calculate_fuel_cost(distance=distance, fuel_type=fuel_type, cost_per_litre=cost_per_litre)
+    return cost
+
+
+# Calculate the emissions saved by not using a car
+def calculate_emissions_saved(distance, car_make, car_model):
+    car_emissions_rate = get_car_emissions_rate()
+    emissions_saved = distance * car_emissions_rate
+    return emissions_saved
