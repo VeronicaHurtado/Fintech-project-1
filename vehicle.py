b7c5268c53d4c1d3087886f295282d94adf55efd
peeetek <peterletran9@gmail.com>
2023-01-16 20:46:20 +1100

    updated code to convert year as integer

--- vehicle.py
+++ vehicle.py
@@ -1,21 +1,29 @@
 import pandas as pd
 import streamlit as st
+import numpy as np
 
 #setting path from csv
 csv_path = ("Resources/vehicles.csv")
 vehicles_df = pd.read_csv(csv_path)
 
+# Replace the non-finite values with -1
+vehicles_df['year'].replace([np.inf, -np.inf], np.nan, inplace=True)
+vehicles_df.fillna(-1, inplace=True)
+
+# Convert the year column to an integer
+vehicles_df['year'] = vehicles_df['year'].astype(int)
+
 # Create a drop-down menu for the year
 year = st.selectbox("Select the year", vehicles_df['year'].unique())
 
 # Create a drop-down menu for the make
-make = st.selectbox("Select the make", vehicles_df['make'].unique())
+make = st.selectbox("Select the make", vehicles_df[vehicles_df['year'] == year]['make'].unique())
 
 # Create a drop-down menu for the model
-model = st.selectbox("Select the model", vehicles_df['model'].unique())
+model = st.selectbox("Select the model", vehicles_df[(vehicles_df['year'] == year) & (vehicles_df['make'] == make)]['model'].unique())
 
 # Create a drop-down menu for the transmission
-transmission = st.selectbox("Select the transmission", vehicles_df['trany'].unique())
+transmission = st.selectbox("Select the transmission", vehicles_df[(vehicles_df['year'] == year) & (vehicles_df['make'] == make) & (vehicles_df['model'] == model)]['trany'].unique())
 
 if st.button("Submit"):
     # Filter the data based on the user's selection
