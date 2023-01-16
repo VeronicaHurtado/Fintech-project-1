e0d1705d3b54daa261fe4701b1ae0920cfa1a9dd
peeetek <peterletran9@gmail.com>
2023-01-16 19:10:57 +1100

    vehicle selection

--- README.md
+++
@@ -1,3 +0,0 @@
-# Fintech-project-1
-
-## Project Overview
---
+++ vehicle.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import streamlit as st
+
+#setting path from csv
+csv_path = ("Resources/vehicles.csv")
+vehicles_df = pd.read_csv(csv_path)
+
+# Create a drop-down menu for the year
+year = st.selectbox("Select the year", vehicles_df['year'].unique())
+
+# Create a drop-down menu for the make
+make = st.selectbox("Select the make", vehicles_df['make'].unique())
+
+# Create a drop-down menu for the model
+model = st.selectbox("Select the model", vehicles_df['model'].unique())
+
+# Create a drop-down menu for the transmission
+transmission = st.selectbox("Select the transmission", vehicles_df['trany'].unique())
+
+if st.button("Submit"):
+    # Filter the data based on the user's selection
+    filtered_df = vehicles_df[(vehicles_df['year'] == year) & (vehicles_df['make'] == make) & (vehicles_df['model'] == model) & (vehicles_df['trany'] == transmission)]
+
+    # Output the fuel efficiency in litres per 100 km - Highway
+    st.write("Fuel efficiency:", filtered_df['UHighway'].values[0], "L/100km")
+    
+    # Output the fuel efficiency in litres per 100 km - City
+    st.write("Fuel efficiency:", filtered_df['UCity'].values[0], "L/100km")
+
+    # output the fuel type and drivetrain
+    st.write("Fuel Type:", filtered_df['FuelType'].values[0])
+    st.write("Drivetrain:", filtered_df['Drivetrain'].values[0])
